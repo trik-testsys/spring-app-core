2ec5ceb3102d2e3657ce5fe813ef70b8345d8acb
Roman Shishkin <st077017@student.spbu.ru>
2025-08-11 06:39:27 +0500

    feat(core-commons): Updated tests

--- core-commons/src/test/kotlin/trik/testsys/sac/TSAppTest.kt
+++ core-commons/src/test/kotlin/trik/testsys/sac/TSAppTest.kt
@@ -1,8 +1,11 @@
 package trik.testsys.sac
 
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import trik.testsys.sac.application.TSApp
+import trik.testsys.sac.data.service.user.UserService
 
 /**
  * @author Roman Shishkin
@@ -10,6 +13,9 @@ import trik.testsys.sac.application.TSApp
  */
 @SpringBootTest(classes = [TSApp::class])
 class TSAppTest {
+
+    @MockitoBean
+    private val userService = mock<UserService<*>>()
 
     @Test
     fun contextLoads() = Unit
